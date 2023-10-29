88784650eae755a9da2bb4b508fbd842ccfb27a4
TheMiranda02 <jose.guzman26@unach.mx>
2023-10-29 15:57:29 -0600

    Agregar archivos de proyecto.

---
+++ TABLASDEMULTIPLICARPYTHON/TABLASDEMULTIPLICARPYTHON.py
@@ -0,0 +1,11 @@
+#ELIAS MIRANDA PYTHON 
+NUM = int(input("DIGITE UN NUMERO: "))
+I = 1
+
+
+while I <= 12:
+    RESUL = NUM * I
+    print(NUM, " * ", I, " = ", RESUL)
+    I += 1 
+
+print("PULSE UNA TECLA")
